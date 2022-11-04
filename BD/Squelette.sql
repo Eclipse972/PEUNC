74ecf5c64afab4e242cae49f2c1df3b6a2855e24
Christophe HERVI <christophe.hervi@free.fr>
2022-11-04 22:20:46 +0100

    Table Squelette avec page d'accueil

--- BD/Squelette.sql
+++ BD/Squelette.sql
@@ -14,15 +14,11 @@ CREATE TABLE IF NOT EXISTS `Squelette` (
   `controleur` varchar(99) collate latin1_general_ci NOT NULL,
   PRIMARY KEY  (`ID`),
   UNIQUE KEY `navigation` (`alpha`,`beta`,`gamma`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=6 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=2 ;
 
 --
 -- Contenu de la table `Squelette`
 --
 
 INSERT INTO `Squelette` (`ID`, `alpha`, `beta`, `gamma`, `texteMenu`, `imageMenu`, `ptiNom`, `classePage`, `controleur`) VALUES
-(1, -1, 500, 0, 'Serveur satur&eacute;', '', 'Serveur_sature', 'PageErreur', 'erreur_serveur.php'),
-(2, -1, 404, 0, 'Cette page n&apos;existe pas', '', 'Page_inexistante', 'PageErreur', 'erreur_serveur.php'),
-(3, -1, 403, 0, 'Acc&egrave;s interdit', '', 'Acces_interdit', 'PageErreur', 'erreur_serveur.php'),
-(4, -1, 0,	 0, 'Erreur inconnue', '', 'Erreur', 'PageErreur', 'erreur_serveur.php'),
-(5,  0, 0,	 0, 'Page d&apos;accueil', '', 'home', 'home', 'home.php');
+(1,  0, 0,	 0, 'Page d&apos;accueil', '', 'home', 'PEUNC\Page', 'home.php');
