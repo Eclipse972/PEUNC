e0a25e6eefdc97541a3dce4925dfc19104fb385b
Christophe HERVI <christophe.hervi@free.fr>
2022-12-28 01:06:46 +0100

    ajout de paramètres

--- BD/Squelette.sql
+++ BD/Squelette.sql
@@ -1,5 +1,28 @@
+-- phpMyAdmin SQL Dump
+-- version 3.1.5
+-- http://www.phpmyadmin.net
 --
--- Structure de la table `Squelette` l'ossature de PEUNC
+-- Serveur: faq.sw.sql.free.fr
+-- Généré le : Mer 28 Décembre 2022 à 00:51
+-- Version du serveur: 5.0.83
+-- Version de PHP: 5.3.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `faq_sw`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Squelette`
 --
 
 CREATE TABLE IF NOT EXISTS `Squelette` (
@@ -12,13 +35,11 @@ CREATE TABLE IF NOT EXISTS `Squelette` (
   `ptiNom` varchar(99) collate latin1_general_ci NOT NULL,
   `classePage` varchar(99) collate latin1_general_ci NOT NULL default 'Page',
   `controleur` varchar(99) collate latin1_general_ci NOT NULL,
+  `methode` varchar(99) collate latin1_general_ci NOT NULL default 'GET',
+  `paramAutorise` varchar(99) collate latin1_general_ci NOT NULL default '[]',
+  `dureeCache` int(11) NOT NULL default '0' COMMENT 'en heure',
   PRIMARY KEY  (`ID`),
   UNIQUE KEY `navigation` (`alpha`,`beta`,`gamma`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
 
---
--- Contenu de la table `Squelette`
---
 
-INSERT INTO `Squelette` (`ID`, `alpha`, `beta`, `gamma`, `texteMenu`, `imageMenu`, `ptiNom`, `classePage`, `controleur`) VALUES
-(1,  0, 0,	 0, 'Page d&apos;accueil', '', 'home', 'PEUNC\Page', 'home.php');
