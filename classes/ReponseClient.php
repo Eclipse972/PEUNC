ddd8558b20b8207c6cf89351451ae0b508417348
Christophe HERVI <christophe.hervi@free.fr>
2022-12-28 01:09:57 +0100

    création de la méthode Page qui gère automatiquement les caches

--- classes/ReponseClient.php
+++ classes/ReponseClient.php
@@ -17,34 +17,40 @@ class ReponseClient
 		 * tousà l'avance.
 		 * La liste des paramètres de chaque page est diponible dans la table squellette
 		 * Une age peut est 'cachée' si la méthode http =GET  et pas de paramètre */
-
 		$this->route = $route;
-
+		
 		/* Remarque: dans le cas d'un traitement de formulaire, la redirection devrait provoquer
 		 * une nouvelle requête qui générera une nouvelle réponse. A VÉRIFIER */
 	}
 
-	public function AvecCache($duree2vie = 3600)
+	public function Page()
 	{
-		$fichierCache = self::DOSSIER_CACHE . "cache" . str_replace('/','-',$this->route->getURL()) . '.html';
-		If(file_exists($fichierCache) && filemtime($fichierCache) + $duree2vie > time())
-		{	// le cache existe et n'est pas périmé
-			$PAGE = new PAGE($route);
-			$PAGE->setView($fichierCache, false);
-		} else
-		{	// il faut créer le cache
-			$PAGE = $this->SansCache();
+		$dureeCache = BDD::SELECT("dureeCache FROM Squelette WHERE alpha=? AND beta=? AND gamma=? AND methode=?",
+				[$this->route->getAlpha(), $this->route->getBeta(), $this->route->getGamma(), $this->route->getMethode()]);
+		if($dureeCache==0)
+			return $this->SansCache();
+		else
+		{	// gestion du cache
+			$fichierCache = self::DOSSIER_CACHE . "cache" . str_replace('/','-',$this->route->URL()) . '.html';
+			If(file_exists($fichierCache) && filemtime($fichierCache) + $dureeCache * 3600 > time())
+			{	// le cache existe et est valide
+				$PAGE = new PAGE($this->route);
+				$PAGE->setView($fichierCache, false);
+			} else
+			{	// il faut créer le cache
+				$PAGE = $this->SansCache();
 
-			// création du cache
-			ob_start();
-			include $PAGE->getView();	// la vue fait appel à une variable $PAGE pour fonctionner
-			$contenu = ob_get_clean();
-			$contenu = str_replace("<body>", "<!-- cache créé le " .  date("d-m-Y") . " à " . date("H:i:s") ." -->\n<body>", $contenu);
-			file_put_contents($fichierCache, $contenu);
+				// création du cache
+				ob_start();
+				include $PAGE->getView();	// la vue fait appel à une variable $PAGE pour fonctionner
+				$contenu = ob_get_clean();
+				$contenu = str_replace("<body>", "<!-- cache créé le " .  date("d-m-Y") . " à " . date("H:i:s") ." -->\n<body>", $contenu);
+				file_put_contents($fichierCache, $contenu);
+			}
+			return $PAGE;
 		}
-		return $PAGE;
 	}
-
+	
 	public function SansCache()
 	{	// trouver les infos pour construire la réponse
 		$Treponse = BDD::SELECT("classePage, controleur, paramAutorise FROM Squelette WHERE alpha=? AND beta=? AND gamma=? AND methode=?",
