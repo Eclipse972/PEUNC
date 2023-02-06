c26784dbf0072d07324ed7785857e2ef4c664743
Christophe HERVI <christophe.hervi@free.fr>
2023-02-06 07:11:37 +0100

    utilisation de Vue_URLvalides pour créer le code de chaque noeud de l'arborescence Squelette

--- BD/Vue_code_item.sql
+++ BD/Vue_code_item.sql
@@ -1,15 +1,15 @@
-# génère le code html qui agrège image+texte
+-- génère le code html qui agrège image+texte pour tous les noeud de l'arborescence
 DROP VIEW IF EXISTS Vue_code_item;
 CREATE VIEW Vue_code_item AS
 SELECT
 	alpha,
 	beta,
 	gamma,
-	(SELECT URL FROM Vue_Routes WHERE niveau1 = alpha AND niveau2 = beta AND niveau3 = gamma AND methodeHttp = 'GET') AS URL,
+	(SELECT URL FROM Vue_URLvalides WHERE niveau1 = alpha AND niveau2 = beta AND niveau3 = gamma) AS URL,
 	imageMenu AS image,
 	texteMenu AS texte,
 	CONCAT(
-		'<a href="',(SELECT URL FROM Vue_Routes WHERE niveau1 = alpha AND niveau2 = beta AND niveau3 = gamma AND methodeHttp = 'GET'),'">',
+		'<a href="',(SELECT URL),'">',
 		IF(imageMenu = '','',CONCAT('<img src="/images/',imageMenu,'" alt="',texteMenu,'">')), #-- code de l'image si elle est définie
 		texteMenu,'</a>'
 	) AS code
