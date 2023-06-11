7f5c6357a6a40a8015a2c7ae0eff8c5df6dcb427
Christophe HERVI <christophe.hervi@free.fr>
2023-06-12 01:51:56 +0200

    liste sous d'une constante de classe

--- Sources/Erreur/Exception.php
+++ Sources/Erreur/Exception.php
@@ -3,47 +3,47 @@ namespace PEUNC\Erreur;
 
 class Exception extends \Exception
 {
+	const LISTE = array(
+		// HttpRoute
+		100 => "M&eacute;thode non prise en charge",
+		101 => "Jeton CSRF inexistant",
+		102 => "Jeton CSRF invalide",
+
+		// ReponseClient
+		200 => "La classe de page n&apos;est pas d&eacute;finie dans le squelette.",
+
+		// Formulaire
+		300 => "Méthode inatendue pour un formulaire",
+		301 => "Configuration chifrement absente",
+
+		// Connexion
+		400 => "Utilisateur en doublon",
+
+		// Page
+		500 => "Vue inexistante",
+		501 => "Controleur non d&eacute;fini",
+		502 => "Controleur inexistant",
+		503 => "fonction MENU: erreur de param&egrave;tre",
+		504 => "fonction MENU: erreur deuxième paramètre",
+
+		// BDD
+		600 => "fichier de config de la base des donn&eacute;es absent",
+		601 => "requ&ecirc;te avec un type de param&egrave;tre non g&eacute;r&eacute;",
+		602 => "Requ&ecirc;te de suppression dangereuse",
+		603 => "les param&egrave;tres sont obligatoires pour l'ajout en BDD",
+		604 => "les param&egrave;tres sont obligatoires pour la mise &agrave; jour en BDD",
+
+		// Utilisateur
+		700 => "Pseudo utilis&eacute; plusieurs fois",
+
+		// inconnue
+		0 => "Erreur inconnue"
+	);
+
 	public function __construct($param = 0)
 	{
-		$T_erreurs = array(
-			// HttpRoute
-			100 => "M&eacute;thode non prise en charge",
-			101 => "Jeton CSRF inexistant",
-			102 => "Jeton CSRF invalide",
-
-			// ReponseClient
-			200 => "La classe de page n&apos;est pas d&eacute;finie dans le squelette.",
-
-			// Formulaire
-			300 => "Méthode inatendue pour un formulaire",
-			301 => "Configuration chifrement absente",
-
-			// Connexion
-			400 => "Utilisateur en doublon",
-
-			// Page
-			500 => "Vue inexistante",
-			501 => "Controleur non d&eacute;fini",
-			502 => "Controleur inexistant",
-			503 => "fonction MENU: erreur de param&egrave;tre",
-			504 => "fonction MENU: erreur deuxième paramètre",
-
-			// BDD
-			600 => "fichier de config de la base des donn&eacute;es absent",
-			601 => "requ&ecirc;te avec un type de param&egrave;tre non g&eacute;r&eacute;",
-			602 => "Requ&ecirc;te de suppression dangereuse",
-			603 => "les param&egrave;tres sont obligatoires pour l'ajout en BDD",
-			604 => "les param&egrave;tres sont obligatoires pour la mise &agrave; jour en BDD",
-
-			// Utilisateur
-			700 => "Pseudo utilis&eacute; plusieurs fois",
-
-			// inconnue
-			0 => "Erreur inconnue"
-		);
-
 		if (is_int($param))
-			$message = isset($T_erreurs[$param]) ? $T_erreurs[$param] : $T_erreurs[0] . " code: " . $param;
+			$message =  array_key_exists($param, LISTE) ? LISTE[$param] : LISTE[0] . " code: " . $param;
 		else
 			$message = $param;
 
--- Sources/Erreur/ServeurException.php
+++ Sources/Erreur/ServeurException.php
@@ -3,16 +3,17 @@ namespace PEUNC\Erreur;
 
 class ServeurException extends \Exception
 {
+	const LISTE = array(
+		403 => "Acc&egrave;s interdit",
+		404 => "Cette page n&apos;existe pas",
+		405 => "M&eacute:thode non prise en charge",
+		500 => "Serveur satur&eacute;"
+		// rajouter ici les nouvelles erreurs reconnues
+	);
+
 	public function __construct($code)
-	{
-		$ListeErreurs = array(
-				403 => "Acc&egrave;s interdit",
-				404 => "Cette page n&apos;existe pas",
-				405 => "M&eacute:thode non prise en charge",
-				500 => "Serveur satur&eacute;"
-				// rajouter ici les nouvelles erreurs reconnues
-			);
-		$message = isset($ListeErreurs[$code]) ? $ListeErreurs[$code] : "Erreur serveur N°" . $code;
+	{		
+		$message = array_key_exists($code, LISTE) ? LISTE[$code] : "Erreur serveur N°" . $code;
 		parent::__construct($message, $code);
 	}
 }
