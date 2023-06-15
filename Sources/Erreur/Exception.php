7850697edf6e7e8d907320717aeb885e106a491d
Christophe HERVI <christophe.hervi@free.fr>
2023-06-15 16:47:43 +0200

    remplacement guillemets + paramètre complément

--- Sources/Erreur/Exception.php
+++ Sources/Erreur/Exception.php
@@ -5,45 +5,48 @@ class Exception extends \Exception
 {
 	const LISTE = array(
 		// HttpRoute
-		100 => "M&eacute;thode non prise en charge",
-		101 => "Jeton CSRF inexistant",
-		102 => "Jeton CSRF invalide",
+		100 => 'M&eacute;thode non prise en charge',
+		101 => 'Jeton CSRF inexistant',
+		102 => 'Jeton CSRF invalide',
 
 		// ReponseClient
-		200 => "La classe de page n&apos;est pas d&eacute;finie dans le squelette.",
+		200 => 'La classe de page n&apos;est pas d&eacute;finie dans le squelette.',
 
 		// Formulaire
-		300 => "Méthode inatendue pour un formulaire",
-		301 => "Configuration chifrement absente",
+		300 => 'Méthode inatendue pour un formulaire',
+		301 => 'Configuration chifrement absente',
 
 		// Connexion
-		400 => "Utilisateur en doublon",
+		400 => 'Utilisateur en doublon',
 
 		// Page
-		500 => "Vue inexistante",
-		501 => "Controleur non d&eacute;fini",
-		502 => "Controleur inexistant",
-		503 => "fonction MENU: erreur de param&egrave;tre",
-		504 => "fonction MENU: erreur deuxième paramètre",
+		500 => 'Vue inexistante',
+		501 => 'Controleur non d&eacute;fini',
+		502 => 'Controleur inexistant',
+		503 => 'fonction MENU: erreur de param&egrave;tre',
+		504 => 'fonction MENU: erreur deuxième paramètre',
 
 		// BDD
-		600 => "fichier de config de la base des donn&eacute;es absent",
-		601 => "requ&ecirc;te avec un type de param&egrave;tre non g&eacute;r&eacute;",
-		602 => "Requ&ecirc;te de suppression dangereuse",
-		603 => "les param&egrave;tres sont obligatoires pour l'ajout en BDD",
-		604 => "les param&egrave;tres sont obligatoires pour la mise &agrave; jour en BDD",
+		600 => 'fichier de config de la base des donn&eacute;es absent',
+		601 => 'requ&ecirc;te avec un type de param&egrave;tre non g&eacute;r&eacute;',
+		602 => 'Requ&ecirc;te de suppression dangereuse',
+		603 => 'les param&egrave;tres sont obligatoires pour l&apos;ajout en BDD',
+		604 => 'les param&egrave;tres sont obligatoires pour la mise &agrave; jour en BDD',
 
 		// Utilisateur
-		700 => "Pseudo utilis&eacute; plusieurs fois",
+		700 => 'Pseudo utilis&eacute; plusieurs fois',
 
 		// inconnue
-		0 => "Erreur inconnue"
+		0 => 'Erreur inconnue'
 	);
 
-	public function __construct($param = 0)
+	public function __construct($param, $complement = '')
 	{
 		if (is_int($param))
-			$message =  array_key_exists($param, self::LISTE) ? self::LISTE[$param] : self::LISTE[0] . " code: " . $param;
+			$message = array_key_exists($param, self::LISTE) ?
+						self::LISTE[$param] : 
+						self::LISTE[0] . ' code: ' . $param;
+			$message .= '<br>' . $complement;
 		else
 			$message = $param;
 
