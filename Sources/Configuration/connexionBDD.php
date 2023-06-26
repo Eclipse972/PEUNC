e96b4244a0fa9fcfe58cb86eb38d23e8400159ed
Christophe HERVI <christophe.hervi@free.fr>
2023-06-26 12:49:40 +0200

    renommage de la classe et du fichier

--- Sources/Configuration/connexionBDD.php
+++ Sources/Configuration/connexionBDD.php
@@ -2,17 +2,17 @@
 namespace PEUNC\Configuration;
 /**
  * Dans votre application il faudra renommer le namespace en App\Configuration par exmemple.
- * Sauvegarder configBDD.php dans le dossier App/Configuragion
- * Ajouter "use App\Configuration\configBDD" en début de script pour utiliser la classe.
+ * Sauvegarder connexionBDD.php dans le dossier App/Configuragion
+ * Ajouter "use App\Configuration\connexionBDD" en début de script pour utiliser la classe.
  * L'autoloader la chargera automatiquement pour vous.
- * Pour utiliser une variable: configBDD::variable. Par exemple configBDD::user.
+ * Pour utiliser une variable: connexionBDD::variable. Par exemple connexionBDD::user.
  *      _
  *     / \
  *    / | \     Ce fichier ne DOIT PAS être suivi par git.
  *   /  |  \    
  *  /___o___\   Pour cela ajouter la ligne: App/Configuration/ dans .gitignore
  */
-class configBDD
+class connexionBDD
 {
     const host = 'serveur.fr';
     const dbname ='base_de_données';
