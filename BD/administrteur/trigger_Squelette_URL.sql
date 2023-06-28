f678e13f51849ac7f6a5b17eae4a74fa34711d1e
Christophe HERVI <christophe.hervi@free.fr>
2023-06-29 00:55:33 +0200

    Trigger chez free.fr

--- BD/administrteur/trigger_Squelette_URL.sql
+++ BD/administrteur/trigger_Squelette_URL.sql
@@ -1,8 +1,12 @@
 -- Crée ou MAJ du champ URL de la table Squelette
--- DROP TRIGGER Squelette_URL; interdit chez free
+-- il ne semble possible de créerun trigger mais il semble inactif et pas possible de voir où il est enregistré
+DROP TRIGGER Squelette_URL; interdit chez free
+DELIMITER $$
 CREATE TRIGGER Squelette_URL AFTER UPDATE ON Squelette
 FOR EACH ROW
-SET URL =(  SELECT Vue_URLvalides.URL
-            FROM Vue_URLvalides
-            WHERE niveau1 = alpha AND niveau2 = beta AND niveau3 = gamma
-        );
+BEGIN
+    SET NEW.URL = ( SELECT URL
+                    FROM Vue_URLvalides
+                    WHERE niveau1 = alpha AND niveau2 = beta AND niveau3 = gamma);
+END
+DELIMITER ;
