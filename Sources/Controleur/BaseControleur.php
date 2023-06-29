2ff02ee5750e6e0bd2b752091404b38b170a1073
Christophe HERVI <christophe.hervi@free.fr>
2023-06-29 02:41:17 +0200

    le dossier CSS ne change pas dans un projet

--- Sources/Controleur/BaseControleur.php
+++ Sources/Controleur/BaseControleur.php
@@ -7,7 +7,8 @@ use PEUNC\Erreur\Exception;
 
 class BaseControleur implements iBaseControleur
 {
-// des listes
+const dossierCSS = 'CSS';
+
 protected $T_element;	// tableau asociatif des éléments simples à afficher (chaîne de caractères ou nombre)
 
 protected $route;	// la route http
@@ -15,10 +16,9 @@ protected $route;	// la route http
 public function __construct(HttpRoute $route = null)
 {
 	$T_element= array(
-		'T_CSS'		=> [],	// liste des feuilles CSS
-		'T_nav'		=> [],	// liste du code pour le menu en liste de listes (ul)
-		'vue'		=> 'doctype.html',
-		'dossierCSS'=> 'CSS'
+		'T_CSS'	=> [],	// liste des feuilles CSS
+		'T_nav'	=> [],	// liste du code pour le menu en liste de listes (ul)
+		'vue'	=> 'doctype.html'
 	);
 	$this->route = $route;
 }
@@ -33,7 +33,7 @@ public function get($clé)
 
 private function AjouteCSS($feuilleCSS)
 {
-	$fichier = '/' . $this->T_element['dossierCSS'] . '/' . $feuilleCSS . '.css';
+	$fichier = '/' . self::dossierCSS . '/' . $feuilleCSS . '.css';
 	// A faire: vérification de l'existence
 	$this->T_element['T_CSS'][] = $fichier;	// ajout d'un CSS à la liste
 }
