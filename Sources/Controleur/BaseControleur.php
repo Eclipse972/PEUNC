642308be2e3fc735cfc688ccd0d2afe0667c7610
Christophe HERVI <christophe.hervi@free.fr>
2023-06-29 02:36:38 +0200

    changement indentation

--- Sources/Controleur/BaseControleur.php
+++ Sources/Controleur/BaseControleur.php
@@ -7,34 +7,34 @@ use PEUNC\Erreur\Exception;
 
 class BaseControleur implements iBaseControleur
 {
-	// des listes
-	protected $T_element;	// tableau asociatif des éléments simples à afficher (chaîne de caractères ou nombre)
-	
-	protected $route;	// la route http
+// des listes
+protected $T_element;	// tableau asociatif des éléments simples à afficher (chaîne de caractères ou nombre)
 
-	public function __construct(HttpRoute $route = null)
-	{
-		$T_element= array(
-			'T_CSS'		=> [],	// liste des feuilles CSS
-			'T_nav'		=> [],	// liste du code pour le menu en liste de listes (ul)
-			'vue'		=> 'doctype.html',
-			'dossierCSS'=> 'CSS'
-		);
-		$this->route = $route;
-	}
-	
-	private function set($nom, $valeur)	 { $this->T_element[$nom] = $valeur; }
+protected $route;	// la route http
 
-	public function get($clé)
-	{
-		if(!array_key_exists($clé, $this->T_element))	throw new Exception('Controleur: clé ' . $clé . ' inexistante');
-		return $this->T_element[$clé];
-	}
-	
-	private function AjouteCSS($feuilleCSS)
-	{
-		$fichier = '/' . $this->T_element['dossierCSS'] . '/' . $feuilleCSS . '.css';
-		// A faire: vérification de l'existence
-		$this->T_element['T_CSS'][] = $fichier;	// ajout d'un CSS à la liste
-	}
+public function __construct(HttpRoute $route = null)
+{
+	$T_element= array(
+		'T_CSS'		=> [],	// liste des feuilles CSS
+		'T_nav'		=> [],	// liste du code pour le menu en liste de listes (ul)
+		'vue'		=> 'doctype.html',
+		'dossierCSS'=> 'CSS'
+	);
+	$this->route = $route;
+}
+
+private function set($nom, $valeur)	 { $this->T_element[$nom] = $valeur; }
+
+public function get($clé)
+{
+	if(!array_key_exists($clé, $this->T_element))	throw new Exception('Controleur: clé ' . $clé . ' inexistante');
+	return $this->T_element[$clé];
+}
+
+private function AjouteCSS($feuilleCSS)
+{
+	$fichier = '/' . $this->T_element['dossierCSS'] . '/' . $feuilleCSS . '.css';
+	// A faire: vérification de l'existence
+	$this->T_element['T_CSS'][] = $fichier;	// ajout d'un CSS à la liste
+}
 }
