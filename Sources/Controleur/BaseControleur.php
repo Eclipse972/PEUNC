3123ed761395a748fe5c26842e557ee384b9e468
Christophe HERVI <christophe.hervi@free.fr>
2023-07-03 19:36:53 +0200

    liste de CSS séparée

--- Sources/Controleur/BaseControleur.php
+++ Sources/Controleur/BaseControleur.php
@@ -12,12 +12,12 @@ const dossierVue = 'Application/Vue';
 
 protected $T_element;	// tableau asociatif des éléments simples à afficher (chaîne de caractères ou nombre)
 protected $vue;			// chemin vers la vue
+protected $T_CSS = [];	// liste des feuilles CSS
 protected $route;	// la route http
 
 public function __construct(HttpRoute $route = null)
 {
 	$T_element= array(
-		'T_CSS'	=> [],	// liste des feuilles CSS
 		'T_nav'	=> [],	// liste du code pour le menu en liste de listes (ul)
 		'vue'	=> 'doctype.html'
 	);
@@ -36,7 +36,7 @@ private function AjouteCSS($feuilleCSS)
 {
 	$fichier = self::dossierCSS . '/' . $feuilleCSS . '.css';
 	// A faire: vérification de l'existence
-	$this->T_element['T_CSS'][] = '/' . $fichier;	// ajout d'un CSS à la liste
+	$this->T_CSS[] = '/' . $fichier;	// ajout d'un CSS à la liste
 }
 
 public function setVue($fichier)
