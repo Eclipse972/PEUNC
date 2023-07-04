6186fe36f7712507790ae1568327c48aefcf0d79
Christophe HERVI <christophe.hervi@free.fr>
2023-07-05 00:18:16 +0200

    indentation

--- Sources/Controleur/Page.php
+++ Sources/Controleur/Page.php
@@ -6,158 +6,159 @@ use PEUNC\Http\HttpRoute;
 use PEUNC\Erreur\Exception;
 use PEUNC\Autre\BDD;
 
-class Page implements iPage	{
+class Page implements iPage
+{
 // CONFIGURATION DE L'APPLICATION
 
-	// dossiers pas défaut. Il devront être déplacés dans un fichier de config ou dans une classe
-	const DOSSIER_VUE		= 'Application/Vue/';
-	const DOSSIER_CONTROLEUR= 'Controleur/';
-	const DOSSIER_CSS		= 'CSS/';
-	// pour le menu
-	const ALPHA_MINI=10;
-	const ALPHA_MAXI=15;
-	
-	// variable membre
-	protected $titrePage	= 'Titre de la page affiché dans la barre du haut du navigateur';
-	protected $T_CSS		= [];
-	protected $entetePage;	// la valeur par défaut est donnée par le champ titre dans le squelette
-	protected $logo			= 'logo.png';
-	protected $dossier		= '/';
-	protected $scriptSection= "<h1>Page en construction</h1>\n<p>Contactez l&apos;adminitrateur si le probl&egrave;me persiste </p>\n";
-	protected $T_nav		= [];
-	protected $PiedDePage	= '<p>Pied de page &agrave; d&eacute;finir</p>';
-	protected $vue;
-	protected $route;
+// dossiers pas défaut. Il devront être déplacés dans un fichier de config ou dans une classe
+const DOSSIER_VUE		= 'Application/Vue/';
+const DOSSIER_CONTROLEUR= 'Controleur/';
+const DOSSIER_CSS		= 'CSS/';
+// pour le menu
+const ALPHA_MINI=10;
+const ALPHA_MAXI=15;
+
+// variable membre
+protected $titrePage	= 'Titre de la page affiché dans la barre du haut du navigateur';
+protected $T_CSS		= [];
+protected $entetePage;	// la valeur par défaut est donnée par le champ titre dans le squelette
+protected $logo			= 'logo.png';
+protected $dossier		= '/';
+protected $scriptSection= "<h1>Page en construction</h1>\n<p>Contactez l&apos;adminitrateur si le probl&egrave;me persiste </p>\n";
+protected $T_nav		= [];
+protected $PiedDePage	= '<p>Pied de page &agrave; d&eacute;finir</p>';
+protected $vue;
+protected $route;
 // FIN DE LA CONFIGURATION
 
-	public function __construct(HttpRoute $route = null)
-	{
-		$this->setView('doctype.html');
-		$this->route = $route;
-		$this->entetePage = isset($route) ? $route->getTitre() : 'Erreur serveur';
-	}
+public function __construct(HttpRoute $route = null)
+{
+	$this->setView('doctype.html');
+	$this->route = $route;
+	$this->entetePage = isset($route) ? $route->getTitre() : 'Erreur serveur';
+}
 /* ***************************
- * MUTATEURS (SETTER)
- * ***************************/
-	public function setTitle($titre)		{ $this->titrePage = $titre; }
+* MUTATEURS (SETTER)
+* ***************************/
+public function setTitle($titre)		{ $this->titrePage = $titre; }
 
-	public function setHeaderText($texte)	{ $this->entetePage = $texte; }
+public function setHeaderText($texte)	{ $this->entetePage = $texte; }
 
-	public function setLogo($logo)			{ $this->logo = $logo; }			// nom de la forme /sous/dossier/fichier.extension à partir du dossier image du site
+public function setLogo($logo)			{ $this->logo = $logo; }			// nom de la forme /sous/dossier/fichier.extension à partir du dossier image du site
 
-	public function setDossier($dossier)	{ $this->dossier = $dossier . '/'; }
+public function setDossier($dossier)	{ $this->dossier = $dossier . '/'; }
 
-	public function setSection($code)		{ $this->scriptSection = $code;	}
+public function setSection($code)		{ $this->scriptSection = $code;	}
 
-	public function setNav(array $code)		{ $this->T_nav = $code;	}
+public function setNav(array $code)		{ $this->T_nav = $code;	}
 
-	public function setFooter($code)		{ $this->PiedDePage = $code; }
+public function setFooter($code)		{ $this->PiedDePage = $code; }
 
-	public function setView($fichier, $cheminParDefaut = true)
+public function setView($fichier, $cheminParDefaut = true)
+{
+	$fichier =  $cheminParDefaut ? self::DOSSIER_VUE . $fichier : $fichier;
+	if (file_exists($fichier))
+		$this->vue = $fichier;
+	else throw new Exception(500);
+}
+
+public function setCSS($feuilleCSS)
+{
+	if(substr($feuilleCSS,0,4) == 'http')
+		$this->T_CSS[] = $feuilleCSS;	// pas de vérification sur feuille externe
+	else
 	{
-		$fichier =  $cheminParDefaut ? self::DOSSIER_VUE . $fichier : $fichier;
-		if (file_exists($fichier))
-			$this->vue = $fichier;
-		else throw new Exception(500);
+		$feuilleCSS = self::DOSSIER_CSS . $feuilleCSS . '.css';
+		if(file_exists($feuilleCSS))
+			$this->T_CSS[] = '/' . $feuilleCSS;
+		else throw new Exception($feuilleCSS . ' n&apos;existe pas');
 	}
+}
 
-	public function setCSS($feuilleCSS)
+/* ***************************
+* ASSESSURS (GETTER)
+* ***************************/
+public function getTitle()			{ return $this->titrePage; }
+
+public function getHeaderText() 	{ return $this->entetePage; }
+
+public function getLogo()			{ return $this->logo; }
+
+public function getDossier()		{ return $this->dossier; }
+
+public function getSection()		{ return $this->scriptSection; }
+
+public function getFooter()			{ return $this->PiedDePage; }
+
+public function getView()			{ return $this->vue; }
+
+public function getCSS()			{ return $this->T_CSS; }
+
+public function getNav()			{ return $this->T_nav; }
+
+public function getRoute()			{ return $this->route; }
+
+/* ***************************
+* AUTRE
+* ***************************/
+
+public function ExecuteControleur($script)
+{
+	if($script == '') throw new Exception(501);
+
+	$script = self::DOSSIER_CONTROLEUR . $script;
+	if (file_exists($script))							//	script défini de manière absolue
+		require($script);
+	else throw new Exception(502, $script);
+}
+
+/* ***************************
+* méthodes statiques
+* ***************************/
+public static function SauvegardeEtat(HttpRoute $route)
+{
+	$URLactuelle = $route->getURL();
+
+	if ($_SESSION['PEUNC']['URL'] != $URLactuelle) // sauvagarde s'il n'y a pas rafraichiisemnt de page
 	{
-		if(substr($feuilleCSS,0,4) == 'http')
-			$this->T_CSS[] = $feuilleCSS;	// pas de vérification sur feuille externe
-		else
+		$_SESSION['PEUNC']['URLprecedente'] = (isset($_SESSION['PEUNC']['URL'])) ? $_SESSION['PEUNC']['URL'] : '/';
+
+		$_SESSION['PEUNC']['URL'] =	$URLactuelle;
+	}
+}
+
+public static function URLprecedente()	{ return $_SESSION['PEUNC']['URLprecedente']; }
+
+public static function MenuAlphaBeta(HttpRoute $route, $alphaMini, $alphaMaxi)
+{
+	$Liste = // recueille la liste des items du menu et du sous-menu
+		BDD::SELECT('	alpha, beta, CONCAT("<li><a href=",URL,">",titre,"</a></li>") AS lien
+						FROM Squelette
+						WHERE (alpha>='.$alphaMini.' AND alpha<='.$alphaMaxi.' AND beta=0 AND gamma=0) OR (alpha=? AND beta>0 AND gamma=0)
+						ORDER BY alpha, beta, gamma',
+						[$route->getAlpha()]);
+	$T_menu = ['<nav>', '<ul>'];
+	for ($i=0; $i < count($Liste); $i++)
+	{ 
+		if ($i>0) // à partir de la 2e ligne
 		{
-			$feuilleCSS = self::DOSSIER_CSS . $feuilleCSS . '.css';
-			if(file_exists($feuilleCSS))
-				$this->T_CSS[] = '/' . $feuilleCSS;
-			else throw new Exception($feuilleCSS . ' n&apos;existe pas');
+			if (($Liste[$i-1]['beta'] == 0) && ($Liste[$i]['beta'] > 0))
+				$T_menu[] = '<ul>';
+			elseif (($Liste[$i-1]['beta'] > 0) && ($Liste[$i]['beta'] == 0))
+				$T_menu[] = '</ul>';
 		}
-	}
-
-/* ***************************
- * ASSESSURS (GETTER)
- * ***************************/
-	public function getTitle()			{ return $this->titrePage; }
-
-	public function getHeaderText() 	{ return $this->entetePage; }
-
-	public function getLogo()			{ return $this->logo; }
-
-	public function getDossier()		{ return $this->dossier; }
-
-	public function getSection()		{ return $this->scriptSection; }
-
-	public function getFooter()			{ return $this->PiedDePage; }
-
-	public function getView()			{ return $this->vue; }
-
-	public function getCSS()			{ return $this->T_CSS; }
-
-	public function getNav()			{ return $this->T_nav; }
-
-	public function getRoute()			{ return $this->route; }
-
-/* ***************************
- * AUTRE
- * ***************************/
-
-	public function ExecuteControleur($script)
-	{
-		if($script == '') throw new Exception(501);
-
-		$script = self::DOSSIER_CONTROLEUR . $script;
-		if (file_exists($script))							//	script défini de manière absolue
-			require($script);
-		else throw new Exception(502, $script);
-	}
-
-/* ***************************
- * méthodes statiques
- * ***************************/
-	public static function SauvegardeEtat(HttpRoute $route)
-	{
-		$URLactuelle = $route->getURL();
-
-		if ($_SESSION['PEUNC']['URL'] != $URLactuelle) // sauvagarde s'il n'y a pas rafraichiisemnt de page
+		$instruction = $Liste[$i]['lien'];
+		if ($Liste[$i]['alpha'] == $route->getAlpha())
 		{
-			$_SESSION['PEUNC']['URLprecedente'] = (isset($_SESSION['PEUNC']['URL'])) ? $_SESSION['PEUNC']['URL'] : '/';
-
-			$_SESSION['PEUNC']['URL'] =	$URLactuelle;
+			if ($Liste[$i]['beta'] == 0)
+				$instruction = str_replace('<a href', '<a id=alpha_actif href' , $instruction);
+			elseif ($Liste[$i]['beta'] == $route->getBeta())
+				$instruction = str_replace('<a href', '<a id=beta_actif href' , $instruction);
 		}
+		$T_menu[] = $instruction;
 	}
-
-	public static function URLprecedente()	{ return $_SESSION['PEUNC']['URLprecedente']; }
-
-	public static function MenuAlphaBeta(HttpRoute $route, $alphaMini, $alphaMaxi)
-	{
-		$Liste = // recueille la liste des items du menu et du sous-menu
-			BDD::SELECT('	alpha, beta, CONCAT("<li><a href=",URL,">",titre,"</a></li>") AS lien
-							FROM Squelette
-							WHERE (alpha>='.$alphaMini.' AND alpha<='.$alphaMaxi.' AND beta=0 AND gamma=0) OR (alpha=? AND beta>0 AND gamma=0)
-							ORDER BY alpha, beta, gamma',
-							[$route->getAlpha()]);
-		$T_menu = ['<nav>', '<ul>'];
-		for ($i=0; $i < count($Liste); $i++)
-		{ 
-			if ($i>0) // à partir de la 2e ligne
-			{
-				if (($Liste[$i-1]['beta'] == 0) && ($Liste[$i]['beta'] > 0))
-					$T_menu[] = '<ul>';
-				elseif (($Liste[$i-1]['beta'] > 0) && ($Liste[$i]['beta'] == 0))
-					$T_menu[] = '</ul>';
-			}
-			$instruction = $Liste[$i]['lien'];
-			if ($Liste[$i]['alpha'] == $route->getAlpha())
-			{
-				if ($Liste[$i]['beta'] == 0)
-					$instruction = str_replace('<a href', '<a id=alpha_actif href' , $instruction);
-				elseif ($Liste[$i]['beta'] == $route->getBeta())
-					$instruction = str_replace('<a href', '<a id=beta_actif href' , $instruction);
-			}
-			$T_menu[] = $instruction;
-		}
-		$T_menu[] = '</ul>';
-		$T_menu[] = '</nav>';
-		return $T_menu;
-	}
+	$T_menu[] = '</ul>';
+	$T_menu[] = '</nav>';
+	return $T_menu;
+}
 }
--- Sources/Http/ReponseClient.php
+++ Sources/Http/ReponseClient.php
@@ -11,79 +11,80 @@ class ReponseClient
  * Classe nécesaire: HttpRoute chargée par l'autoloader
 */
 {
-	const DOSSIER_CACHE = "cache/";
+const DOSSIER_CACHE = "cache/";
 
-	protected $controller;
+protected $controller;
 
-	public function __construct($controller)
+public function __construct($controller)
+{
+	/* pour le moment toutes les pages peuvent être mise en cache.
+		* Or ce pas possible pour les pages posesdant des paramètres car on ne peut les connaitre
+		* tousà l'avance.
+		* La liste des paramètres de chaque page est diponible dans la table squellette
+		* Une age peut est 'cachée' si la méthode http =GET  et pas de paramètre */
+	$this->controller = $controller;
+
+	/* Remarque: dans le cas d'un traitement de formulaire, la redirection devrait provoquer
+		* une nouvelle requête qui générera une nouvelle réponse. A VÉRIFIER */
+}
+
+public function View()
+{
+	return $this->controller->getView();
+}
+
+// balise générique
+private static function Balise($balise, $contenu, $avecBalise)
+{
+	if($avecBalise)	echo "<", $balise, ">\n";
+	echo  $contenu, "\n";
+	if($avecBalise)	echo "</", $balise, ">\n";
+}
+
+// écriture des balises pour la vue
+public function Title()
+{
+	echo $this->controller->getTitle();
+}
+
+public function HeaderText()
+{
+	echo $this->controller->getHeaderText(), "\n";
+}
+
+public function Logo()
+{
+	echo Balise::Image($this->controller->getLogo(),'Logo');
+}
+
+// pas d'implémentation de getDossier()?
+
+public function CSS()
+{
+	$Liste = $this->controller->getCSS();
+
+	foreach($Liste as $feuilleCSS)
+		echo "\t" , '<link rel="stylesheet" href="', $feuilleCSS, '"/>', "\n";
+}
+
+public function Section($avecBalise = false)
+{
+	self::Balise('section', $this->controller->getSection(), $avecBalise);
+}
+
+public function Footer($avecBalise = false)
+{
+	self::Balise('footer', $this->controller->getFooter(), $avecBalise);
+}
+
+public function getMenu($alphaMini, $alphaMaxi)
+{
+	$Liste = Page::MenuAlphaBeta($this->controller->getRoute(), $alphaMini, $alphaMaxi);
+	$code = '';
+	foreach ($Liste as $ligne)
 	{
-		/* pour le moment toutes les pages peuvent être mise en cache.
-		 * Or ce pas possible pour les pages posesdant des paramètres car on ne peut les connaitre
-		 * tousà l'avance.
-		 * La liste des paramètres de chaque page est diponible dans la table squellette
-		 * Une age peut est 'cachée' si la méthode http =GET  et pas de paramètre */
-		$this->controller = $controller;
-
-		/* Remarque: dans le cas d'un traitement de formulaire, la redirection devrait provoquer
-		 * une nouvelle requête qui générera une nouvelle réponse. A VÉRIFIER */
+		$code .= $ligne . "\n";
 	}
-
-	public function View()
-	{
-		return $this->controller->getView();
-	}
-
-	// balise générique
-	private static function Balise($balise, $contenu, $avecBalise)
-	{
-		if($avecBalise)	echo "<", $balise, ">\n";
-		echo  $contenu, "\n";
-		if($avecBalise)	echo "</", $balise, ">\n";
-	}
-	
-	// écriture des balises pour la vue
-	public function Title()
-	{
-		echo $this->controller->getTitle();
-	}
-
-	public function HeaderText()
-	{
-		echo $this->controller->getHeaderText(), "\n";
-	}
-
-	public function Logo()
-	{
-		echo Balise::Image($this->controller->getLogo(),'Logo');
-	}
-
-	// pas d'implémentation de getDossier()?
-
-	public function CSS()
-	{
-		$Liste = $this->controller->getCSS();
-
-		foreach($Liste as $feuilleCSS)
-			echo "\t" , '<link rel="stylesheet" href="', $feuilleCSS, '"/>', "\n";
-	}
-
-	public function Section($avecBalise = false)
-	{
-		self::Balise('section', $this->controller->getSection(), $avecBalise);
-	}
-	
-	public function Footer($avecBalise = false)
-	{
-		self::Balise('footer', $this->controller->getFooter(), $avecBalise);
-	}
-
-	public function getMenu($alphaMini, $alphaMaxi)
-	{
-		$Liste = Page::MenuAlphaBeta($this->controller->getRoute(), $alphaMini, $alphaMaxi);
-		$code = '';
-		foreach ($Liste as $ligne) {
-			$code .= $ligne . "\n";
-		}
-		return $code;
-	}
+	return $code;
+}
 }
