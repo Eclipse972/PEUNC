0cd5217b16f103afaac5de5935c5c056deaf2723
Christophe HERVI <christophe.hervi@free.fr>
2023-08-05 18:11:07 +0200

    vue pour le ste

--- BD/Vue_route.sql
+++ BD/Vue_route.sql
@@ -13,18 +13,20 @@ CONCAT('/',
     -- niveau alpha
 	(SELECT ptiNom 
         FROM Squelette AS T1 
-        WHERE T1.alpha = Squelette.alpha AND T1.beta=0 AND T1.gamma=0 AND T1.methodeHttp='GET'),
+        WHERE T1.alpha = Squelette.alpha AND T1.beta=0 AND T1.gamma=0 
+            AND T1.methodeHttp='GET' AND site='site'),
     -- niveau beta
 	IFNULL((SELECT CONCAT('/',ptiNom) 
             FROM Squelette AS T2 
             WHERE T2.alpha = Squelette.alpha AND T2.beta = Squelette.beta 
-                AND T2.beta > 0 AND T2.gamma = 0 AND T2.methodeHttp = 'GET')
+                AND T2.beta > 0 AND T2.gamma = 0 AND T2.methodeHttp = 'GET' AND site='site')
         ,''),
     -- niveau gamma
 	IFNULL((SELECT CONCAT('/',ptiNom) 
         FROM Squelette AS T3 
         WHERE T3.alpha = Squelette.alpha AND T3.beta = Squelette.beta AND T3.gamma = Squelette.gamma 
-            AND T3.beta>0 AND gamma>0 AND T3.methodeHttp='GET'), '')
+            AND T3.beta>0 AND gamma>0 AND T3.methodeHttp='GET' AND site='site')
+        , '')
 ) AS URL,
 methodeHttp, titre, paramAutorise, controleur, methodeControleur
 FROM Squelette
