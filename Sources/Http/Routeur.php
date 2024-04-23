3c19249bec6696d695694a6fb9e71c74e8caee04
Christophe HERVI <christophe.hervi@free.fr>
2024-04-23 02:13:34 +0200

    ébauche de routeur

---
+++ Sources/Http/Routeur.php
@@ -0,0 +1,55 @@
+<?php
+namespace PEUNC\Http;
+use PEUNC\Http\RequeteHttp;
+use PEUNC\Erreur\ServeurException;
+
+class Routeur {
+private array $Troute = [];
+
+private function ajouteRoute(string $URL,
+							string $methodeHttp,
+							string $namespaceControleur,
+							string $controleurMethode,
+							array $parametreAutorisés) : void 
+{
+	/**
+	 * À FAIRE: vérifier la validité des paramètre
+	 * URL: URL sur 3 niveaux ne contenant que des caractères cf .htacces 
+	 * methodeHttp, namespaceControleur, controleurMethode des chaine de caractères non vide
+	 * lancer un exception si ça arrive
+	 */
+	$this->Troute[$URL][$methodeHttp] = array(
+		'controleur' => $namespaceControleur,
+		'fonction' => $controleurMethode,
+		'paramAutorise' => $parametreAutorisés
+	);
+}
+
+# fonctions pour créer les routes de type GET et POST
+public function get(string $URL,
+					string $namespaceControleur,
+					string $controleurMethode,
+					array $parametreAutorisés = []) : void
+{ $this->ajouteRoute($URL, 'GET', $namespaceControleur, $controleurMethode, $parametreAutorisés); }
+
+public function post(string $URL,
+					string $namespaceControleur,
+					string $controleurMethode,
+					array $parametreAutorisés = []) : void
+{ $this->ajouteRoute($URL, 'POST', $namespaceControleur, $controleurMethode, $parametreAutorisés); }
+
+public function TrouveChemin(RequeteHttp $requete) : array {
+	/**
+	 * C'est index.php qui va instancié le controleur. Cette fonction lui fournit tous les éléments pour le faire
+	 * controleur, sa fonction, la liste des paramètre autorisés et la route
+	 **/
+	$URL = $requete->getURL();
+	$methodeHttp = $requete->getMethode();
+	if ((!array_key_exists($URL, $this->Troute)) # url inexistante
+		|| (!array_key_exists($methodeHttp, $this->Troute[$URL])) # méthode http inconnue pour l'url donnée
+	) throw new ServeurException(404);
+	$T = $this->Troute[$URL][$methodeHttp];
+	#$T['route'] = new httpRoute($requete, $T['paramAutorise']); # route à construire 
+	return $T;
+}
+}
