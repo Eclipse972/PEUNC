defd1fa71254e09182a91e8d2aa5ba5259d68652
Christophe HERVI <christophe.hervi@free.fr>
2024-04-26 04:34:35 +0200

    modele de routeur principal issu de VolÉval

--- Public/index.php
+++ Public/index.php
@@ -1,74 +1,59 @@
-<?php	// routeur de PEUNC
+<?php	# controleur principal
 session_start();
 
-spl_autoload_register(function($classe)
-	{	// $classe contient le namespace qui est aussi le chemin vers le fichier
-		$TcheminFichier = explode('\\', $classe);
+use PEUNC\Http\ReponseClient;
+use PEUNC\Http\HttpRoute;
+use PEUNC\Http\RequeteHttp;
+use PEUNC\Http\Routeur;
+use PEUNC\Erreur\Exception as PEUNC_Exception;
+use PEUNC\Erreur\ServeurException;
+use VolEval\Controleur\ErreurControleur;
 
-		switch($TcheminFichier[0]) // inspection du premier élément
-		{
-			case 'PEUNC':
-				break;
-			case 'VolEval':
-				$TcheminFichier[0] = 'Application';
-				break;
-			default:
-				throw new PEUNC\Erreur\Exception('namespace inconnu: ' . $TcheminFichier[0]);
-		}
-		$fichier = implode('/', $TcheminFichier) . '.php';
-		if(!file_exists($fichier)) throw new PEUNC\Erreur\Exception('autoloader. fichier=' . $fichier);
-		require_once $fichier;
-	}
-);
+include 'autoloader.php';
 
 try
 {
-	$route = new PEUNC\Http\HttpRoute;				// à partir d'une requête Http on trouve la route
+	if (array_key_exists('serverError', $_GET)) # Cf .htaccess pour redirection des erreurs serveurs
+		throw new ServeurException(intval($_GET['serverError']));
 
-	PEUNC\Controleur\Page::SauvegardeEtat($route);	// sauvegarde de l'état courant
+	$requete = new RequeteHttp;
 
-	$controleur = $route->getControleur();
-	if (!isset($controleur))	throw new PEUNC\Erreur\Exception(200);
-	$PAGE = new $controleur($route);	// création de la page
-	$PAGE->ExecuteControleur($route->getFonction());
-	
+	$routeur = new Routeur('VolEval\Controleur');
+	include 'Application/Configuration/routes.php';
+	$T = $routeur->TrouveChemin($requete);
+
+	$controleurNom = $T['controleur'];
+	$controleurMethode = $T['fonction'];
+	$route = $T['route'];
 }
-catch(PEUNC\Erreur\ServeurException $e)
+catch(ServeurException $e)
 {
-	$PAGE = new PEUNC\Controleur\Erreur();	// il n'y a pas de route
-	$PAGE->setTitle("Erreur serveur");
-	$PAGE->setHeaderText("<p>Erreur serveur</p>");
-	$PAGE->setSection("<h1>" . $e->getMessage() . " - code: " . $e->getCode() . "</h1>\n<img src=/images/serveur.png style=\"width:300px\" alt=\"Logo\" >");
-	$PAGE->setFooter("");
-	$PAGE->setView("doctype.html");
+	$route = new HttpRoute($requete, []);
+	$controleurNom = 'Serveur';
+	$controleurMethode = 'Serveur';
 }
 catch(PDOException $e)
 {
-	$PAGE = new PEUNC\Controleur\Erreur($route);
-	$PAGE->setTitle("Erreur de BDD");
-	$PAGE->setHeaderText("<p>Erreur de base de donn&eacute;es</p>");
-	$PAGE->setSection("<p>" . $e->getMessage() . "</p>\n" . $PAGE->NoeudArborescence());
-	$PAGE->setFooter("");
-	$PAGE->setView("doctype.html");
+	$controleurNom = 'PDO';
+	$controleurMethode = 'BDD';
 }
-catch(PEUNC\Erreur\Exception $e)
+catch(PEUNC_Exception $e)
 {
-	$PAGE = new PEUNC\Controleur\Erreur($route);
-	$PAGE->setTitle("Erreur de l&apos;application");
-	$PAGE->setHeaderText("<p>Erreur de l&apos;application</p>");
-	$PAGE->setSection("<h1>" . $e->getMessage() . "</h1>\n" . $PAGE->NoeudArborescence());
-	$PAGE->setFooter("");
-	$PAGE->setView("doctype.html");
+	$controleurNom = 'PEUNC';
+	$controleurMethode = 'Application';
 }
 catch(Exception $e)
 {
-	$PAGE = new PEUNC\Controleur\Erreur($route);
-	$PAGE->setTitle("Erreur inconnue");
-	$PAGE->setHeaderText("<p>Erreur inconnue</p>");
-	$PAGE->SetSection("<h1>" . $e->getMessage() . "</h1>\n" . $PAGE->NoeudArborescence());
-	$PAGE->setFooter("");
-	$PAGE->setView("doctype.html");
+	$controleurNom = 'Exception';
+	$controleurMethode = 'Exception';
 }
 
-$reponse = new PEUNC\Http\ReponseClient($PAGE);// construction de la réponse en fonction de la route trouvée
-include $reponse->View(); //$reponse->Afficher(); // affichage de la vue (réponse client ou page d'erreur)
+# création du controleur
+if (in_array($controleurNom, ['Serveur', 'PDO', 'PEUNC', 'Exception'])) {
+	$controleur = new ErreurControleur($route, $e->getMessage(), $e->getCode());
+} else $controleur = new $controleurNom($route);
+
+$controleur->$controleurMethode(); # application de la méthode au controleur
+
+ReponseClient::Absorbe($controleur); # préparation de la réponse envoyée au client à partir du controleur
+include ReponseClient::View(); # insertion de la vue
